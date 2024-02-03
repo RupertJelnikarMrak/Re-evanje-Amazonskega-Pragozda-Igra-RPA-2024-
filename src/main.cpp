736dc2cf9f95d7b52d2b1ea0c10f5e2b8b697b21
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-02-04 00:25:41 +0100

    Add Game class and refactor main function

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,14 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
 
+#include "Game.hpp"
+
 int main(int argc, char *argv[])
 {
     spdlog::set_level(spdlog::level::debug);
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
 
-    spdlog::info("Initializing SDL2");
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        spdlog::critical("Failed to initialize SDL: ", SDL_GetError());
-        return 1;
-    }
+    Game saving_amazon;
 
-    spdlog::info("Initializing SDL Window");
-    SDL_Window *window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        spdlog::critical("Failed to create window: ", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-
-    spdlog::info("Initializing SDL Renderer");
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        spdlog::critical("Failed to create renderer: ", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    while (window != nullptr);
-    spdlog::info("Exiting");
-    SDL_Quit();
-    return 0;
+    return saving_amazon.init();
 }
