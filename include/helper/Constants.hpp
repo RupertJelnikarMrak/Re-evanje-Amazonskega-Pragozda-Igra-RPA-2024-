2c6187e7b8b86a53ba2c05608ab8760067c7d186
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-02-10 00:34:15 +0100

    Add Constants and Logging files

---
+++ include/helper/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace Const
+{
+    void init();
+    std::string EXE_PATH;
+};
---
+++ src/logging/Logging.cpp
@@ -0,0 +1,42 @@
+#include "logging/Logging.hpp"
+#include "helper/Constants.hpp"
+
+#include <fstream>
+#include <chrono>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+
+bool fileExists(const std::string &pFileName)
+{
+    std::ifstream file(pFileName);
+    return file.good();
+}
+
+void Logging::init()
+{
+    //auto time = std::chrono::system_clock::now();
+    // TODO: Change the file cycle to archive old logs in timed zip format
+
+    std::string logsPath = Const::EXE_PATH + "\\logs\\";
+
+    // Cycle through the log files
+    if (fileExists(logsPath + "Log5.txt"))
+        std::remove((logsPath + "Log5.txt").c_str());
+
+    for (int i = 4; i > 0; i--)
+        if (fileExists(logsPath + "Log" + std::to_string(i) + ".txt"))
+            std::rename((logsPath + "Log" + std::to_string(i) + ".txt").c_str(), (logsPath + "Log" + std::to_string(i + 1) + ".txt").c_str());
+
+    if (fileExists(logsPath + "Latest.txt"))
+        std::rename((logsPath + "Latest.txt").c_str(), (logsPath + "Log1.txt").c_str());
+
+    auto logger = spdlog::basic_logger_mt("game_file_logger", (logsPath + "Latest.txt").c_str());
+    
+    spdlog::set_default_logger(logger);
+    spdlog::flush_on(spdlog::level::info);
+    spdlog::set_level(spdlog::level::info);
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+
+    spdlog::info("Logger initialized!");
+}
