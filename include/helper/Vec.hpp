2ce58342a718482955a5db0034f6612217198fba
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-02-15 23:28:15 +0100

    Refactor Vec2 class in Vec.hpp

--- include/helper/Vec.hpp
+++ include/helper/Vec.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
-template<typename T>
-struct Vec2
+#include <ostream>
+
+template <typename T>
+class Vec2
 {
+public:
     T x, y;
+
+    Vec2(int pX, int pY) : x(pX), y(pY) {}
+
+    template <typename OStream>
+    inline OStream &operator<<(OStream &os, const Vec2<T> &vec)
+    {
+        return os << "X: " << vec.x << ", Y: " << vec.y;
+    }
 };
