245e1c6cff9888eaaa9b8be31168dd14c4a01c4f
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-02-15 23:28:08 +0100

    Refactor RenderWindow class and add new functions

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
+#include "helper/Vec.hpp"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 class RenderWindow
 {
+private:
+    SDL_Window *_window;
+    SDL_Renderer *_renderer;
+    int _width;
+    int _height;
+    int _x;
+    int _y;
+    bool _isFullscreen;
+
 public:
-    RenderWindow(const char* pTitle, int pW, int pH, int pX, int pY, bool pFullscreen);
-    RenderWindow(const char* pTitle, int pW, int pH, bool pFullscreen);
+    RenderWindow(const char *pTitle, int pW, int pH, int pX, int pY, bool pFullscreen);
+    RenderWindow(const char *pTitle, int pW, int pH, bool pFullscreen);
+
+    ~RenderWindow();
+
+    void setSize(Vec2<int> pSize);
+    Vec2<int> getSize() const;
+
+    void setPosition(Vec2<int> pPosition);
+    Vec2<int> getPosition() const;
+
+    void setFullscreen(bool pFullscreen);
+    bool isFullscreen() const;
+
+    /*-------------------------------------------------------*/
+
+    SDL_Texture *loadTexture(const char *pPath);
+    void drawTexture(SDL_Texture *pTexture, Vec2<int> pPos, Vec2<int> pSrcSize, Vec2<int> pDstSize);
 };
--- src/RenderWindow.cpp
+++ src/RenderWindow.cpp
@@ -1,11 +1,75 @@
 #include "RenderWindow.hpp"
 
-RenderWindow::RenderWindow(const char* pTitle, int pW, int pH, int pX, int pY, bool pFullscreen)
-{
+#include "helper/Vec.hpp"
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <spdlog/spdlog.h>
+
+SDL_Texture *RenderWindow::loadTexture(const char *pPath)
+{
+    spdlog::debug("RenderWindow::loadTexture: {}", pPath);
+    return IMG_LoadTexture(_renderer, pPath);
 }
 
-RenderWindow::RenderWindow(const char* pTitle, int pW, int pH, bool pFullscreen)
-{
+// Setters and Getters
 
+void RenderWindow::setSize(Vec2<int> pSize)
+{
+    spdlog::debug("RenderWindow::setSize: {}", pSize);
+    _width = pSize.x;
+    _height = pSize.y;
+    SDL_SetWindowSize(_window, _width, _height);
+}
+
+Vec2<int> RenderWindow::getSize() const
+{
+    return Vec2<int>(_width, _height);
+}
+
+void RenderWindow::setPosition(Vec2<int> pPosition)
+{
+    spdlog::debug("RenderWindow::setPosition: {}", pPosition);
+    _x = pPosition.x;
+    _y = pPosition.y;
+    SDL_SetWindowPosition(_window, _x, _y);
+}
+
+Vec2<int> RenderWindow::getPosition() const
+{
+    return Vec2<int>(_x, _y);
+}
+
+void RenderWindow::setFullscreen(bool pFullscreen)
+{
+    _isFullscreen = pFullscreen;
+    SDL_SetWindowFullscreen(_window, _isFullscreen ? SDL_WINDOW_FULLSCREEN : 0);
+}
+
+bool RenderWindow::isFullscreen() const
+{
+    return _isFullscreen;
+}
+
+// Constructors and Destructors
+
+RenderWindow::RenderWindow(const char *pTitle, int pW, int pH, int pX, int pY, bool pFullscreen)
+    : _width(pW), _height(pH), _x(pX), _y(pY), _isFullscreen(pFullscreen)
+{
+    _window = SDL_CreateWindow(pTitle, pX, pY, pW, pH, pFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN);
+    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
+
+RenderWindow::RenderWindow(const char *pTitle, int pW, int pH, bool pFullscreen)
+    : _width(pW), _height(pH), _isFullscreen(pFullscreen)
+{
+    _window = SDL_CreateWindow(pTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, pW, pH, pFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN);
+    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
+
+RenderWindow::~RenderWindow()
+{
+    SDL_DestroyRenderer(_renderer);
+    SDL_DestroyWindow(_window);
 }
